0dfc86b23c93f4c5fc8e655a507ce54b38f6e0de
Tony Kwok <holypaladin07@gmail.com>
2019-03-05 00:22:37 -0800

    watch for state changes

--- src/Deck.js
+++ src/Deck.js
@@ -50,6 +50,12 @@ export default class Deck extends Component {
         this.state = { panResponder, position, index: 0 };
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.data !== this.props.data) {
+            this.setState({ index: 0 });
+        }
+    }
+
     componentWillUpdate() {
         UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
         LayoutAnimation.spring();
